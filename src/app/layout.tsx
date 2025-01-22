6520b589968d8081abe5ac5cf2cb4c281043bcbd
Wasif Khan <wasifkhan5613@gmail.com>
2025-01-22 02:38:54 -0500

    Initilize Convex with messages API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 
 
@@ -17,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       >
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
